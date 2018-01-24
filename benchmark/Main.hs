048132abac8434b062aa9abd90497caa39727efe
Eric Campbell <1234goal@gmail.com>
2018-01-23 21:53:14 -0500

    Include Automatark for benchmarking

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,6 +1,17 @@
 -- You can benchmark your code quickly and effectively with Criterion. See its
 -- website for help: <http://www.serpentine.com/criterion/>.
 import Criterion.Main
+import Tableau
+
+bchmks :: IO String 
+bchmks = do
+  
+  
 
 main :: IO ()
-main = defaultMain [bench "const" (whnf const ())]
+main = defaultMain [
+  bgroup "Tableau Decision Procedure (sat)" [
+                                              ],
+  bgroup "Tableau Decision Procedure (valid)" [
+                                                ]
+  ]
